6b47fad25aed7b6163e4eb07de643dbf8ef384d2
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-15 14:05:05 -0300

    feat: Criação do footer
    Criado o footer que irá estar presente em todas as páginas do site.

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import Cemitery from "@/assets/img/cemit.png";
+import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import { FaInstagram, FaFacebook, FaTiktok, FaXTwitter } from "react-icons/fa6";
 
-export const Footer: React.FC = () => {
-  return (
-    <div className='text-white'>Footer</div>
-  )
-}
+export const Footer = () => {
+	const { t } = useTranslation();
+
+	return (
+		<footer className="w-full footer">
+			<img src={Cemitery} alt="Cemitery" className="w-full" />
+			<div className="sub-footer flex items-center flex-col w-full">
+				<div className="flex gap-2 w-[1080px] justify-between mb-12">
+					<h6>{t("Hello Hallow")}</h6>
+					<div className="flex flex-col gap-2">
+						<p>{t("Festival")}</p>
+						<NavLink to="/" className="link">
+							{t("About")}
+						</NavLink>
+						<NavLink to="/" className="link">
+							{t("Tickets")}
+						</NavLink>
+					</div>
+					<div className="flex flex-col gap-2">
+						<p>{t("Contact")}</p>
+						<NavLink to="/" className="link">
+							{t("Faq")}
+						</NavLink>
+						<NavLink to="/" className="link">
+							{t("Support")}
+						</NavLink>
+					</div>
+					<div>
+						<p className="mb-2">{t("Social Networks")}</p>
+						<div className="flex gap-2">
+							<NavLink to="/" className="social">
+								<FaInstagram />
+							</NavLink>
+							<NavLink to="/" className="social">
+								<FaFacebook />
+							</NavLink>
+							<NavLink to="/" className="social">
+								<FaTiktok />
+							</NavLink>
+							<NavLink to="/" className="social">
+								<FaXTwitter />
+							</NavLink>
+						</div>
+					</div>
+					<div className="flex flex-col items-end gap-2 ml-16">
+						<p>{t("Join our newsletter")}</p>
+						<form className="form flex rounded-full p-1">
+							<input type="email" placeholder={t("Enter your email")} />
+							<button>{t("Subscribe")}</button>
+						</form>
+					</div>
+				</div>
+				<div className="copyright w-full flex justify-center">
+					<div className="w-[1080px] flex justify-end my-8">
+						<span>{t("© Treat or Trick Festival. All rights reserved.")}</span>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
