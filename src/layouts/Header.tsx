ad11aa021388d13ca2722ace42491cd570eb9d3a
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-15 14:21:53 -0300

    fix: Correção da indentação do header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -9,17 +9,21 @@ export const Header: React.FC = () => {
 	return (
 		<div className="flex items-center justify-between my-12">
 			<span className="uppercase font-outfit text-pumpkin-dark font-bold text-lg">
-				{t('trickortreat')}
+				{t("trickortreat")}
 			</span>
 			<div className="flex gap-4 items-center">
 				<div className="flex gap-4 text-pumpkin-dark uppercase py-2 px-4 rounded-lg nav-menu backdrop-blur-md">
-					<NavLink to="/">{t('about')}</NavLink>
-					<NavLink to="/">{t('lineup')}</NavLink>
-					<NavLink to="/">{t('tickets')}</NavLink>
-					<NavLink to="/">{t('support')}</NavLink>
+					<NavLink to="/">{t("about")}</NavLink>
+					<NavLink to="/">{t("lineup")}</NavLink>
+					<NavLink to="/">{t("tickets")}</NavLink>
+					<NavLink to="/">{t("support")}</NavLink>
 				</div>
-				<button type="button" onClick={handleChangeLanguage} className="w-7 h-fit rounded-full overflow-hidden">
-					<img src={Flag} alt="Translate"/>
+				<button
+					type="button"
+					onClick={handleChangeLanguage}
+					className="w-7 h-fit rounded-full overflow-hidden"
+				>
+					<img src={Flag} alt="Translate" />
 				</button>
 			</div>
 		</div>
