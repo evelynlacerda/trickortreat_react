123858ff6acd7cbeef37e7089cd174e4c2fd6438
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-15 13:53:14 -0300

    Correções e incrementos
    fix: Correção do caminho para Index.tsx
    - O caminho para o arquivo `Index.tsx` foi corrigido dentro de
      `Router.tsx`.
    
    feat: Inserção da nova lib
    - A lib `Motion` foi adicionada no arquivo `Index.tsx` para lidar com
      o movimento do personagem Jack que simula uma flutuação.

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { useTranslateOnly } from "@/lib/languageUtils";
+import { motion } from "framer-motion";
 
 // Images
 import Background from "@/assets/img/top-bg.png";
@@ -12,8 +12,18 @@ import PartyThree from "@/assets/img/party3.png";
 // Styles
 import "@/assets/css/style.css";
 
-export const Index: React.FC = () => {
+export const Index = () => {
 	const { t } = useTranslateOnly();
+
+	const floatJackAnimation = {
+		y: [-10, 40, -10],
+        x: [-10, 60, -10],
+		transition: {
+			duration: 2,
+			repeat: Infinity,
+			repeatType: "loop",
+		},
+	};
 
 	return (
 		<>
@@ -24,15 +34,19 @@ export const Index: React.FC = () => {
 				</div>
 				<div className="w-[1080px]">
 					<div className="flex items-center justify-between -mt-[180px]">
-						<img src={Jack} alt="Jack Pumpkin" />
+						<motion.img
+							src={Jack}
+							alt="Jack Pumpkin"
+							animate={floatJackAnimation}
+						/>
 						<div className="flex flex-col items-center gap-14">
 							<div className="flex flex-col font-outfit text-pumpkin-dark uppercase font-bold">
-								<p className="text-[32px]">{t('thebest')}</p>
-								<p className="text-[56px] -mt-4">{t('halloween')}</p>
-								<p className="text-[32px] text-end -mt-4">{t('party')}</p>
+								<p className="text-[32px]">{t("thebest")}</p>
+								<p className="text-[56px] -mt-4">{t("halloween")}</p>
+								<p className="text-[32px] text-end -mt-4">{t("party")}</p>
 							</div>
-							<div className="flex items-center gap-8">
-								<div className="flex flex-col gap-8">
+							<div className="flex items-center gap-10">
+								<div className="flex flex-col gap-10">
 									<img src={PartyOne} alt="Party One" />
 									<img src={PartyTwo} alt="Party Two" />
 								</div>
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Structor } from '../layouts/Structor'
-import { Index } from '@/pages/Index'
+import { Index } from '@/components/Index'
 
 export const Router: React.FC = () => {
   return (
